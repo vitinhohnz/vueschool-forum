39e8fd43b35978d83aec4f672fe684ecbed1b870
Victor Hernandez <victor@yampi.com.br>
2022-03-03 22:23:48 -0300

    displaying a list of elements on the homepage

---
+++ src/components/PageHome.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>Hello from Page Home</div>
+  <div v-for="thread in threads" :key="thread.id">
+    <h2>{{ thread.title }}</h2>
+    <div v-for="postId in thread.posts" :key="postId">
+      <p>{{ userById(postById(postId).userId).name }}</p>
+      <p>{{ postById(postId).text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+
+export default {
+  data () {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  },
+  methods: {
+    postById (postId) {
+      return this.posts.find(p => p.id === postId)
+    },
+    userById (userId) {
+      return this.users.find(u => u.id === userId)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
